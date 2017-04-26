b4fec62e50725fbd2b282b54f1266d6a0670ceb8
root <sunilm807@gmail.com>
2017-04-26 10:29:58 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+using Amazon.Lambda.Core;
+
+[assembly: LambdaSerializerAttribute(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
+
+namespace basic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World! ABC");
+        }
+
+        public string FunctionHandler(string input, ILambdaContext context)
+        {
+            return input?.ToUpper();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+## Deploy lambda functions to AWS
+=================================
+
+# Overview
+ - Automatically Build and deploy AWS Functions
